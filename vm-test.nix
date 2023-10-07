aeae64110fa6e9b9f8877ecfeb322c5642e2d4da
car <carl.hedgren@gmail.com>
2023-10-07 21:22:45 +0200

    add some datas

--- vm-test.nix
+++ vm-test.nix
@@ -54,39 +54,43 @@ in {
     };
 
     testScript = ''
-       import json
-       import shlex
-       createReadSA = json.dumps({
-                  "name": "newGrafanaSA",
-                  "role": "Viewer",})
-       createReadWriteSA = json.dumps({
-                  "name": "newGrafanaSA",
-                  "role": "Editor",})
+      import json
+      import shlex
+      createReadSA = json.dumps({
+                 "name": "newGrafanaSA",
+                 "role": "Viewer",})
+      createReadWriteSA = json.dumps({
+                 "name": "newGrafanaSA",
+                 "role": "Editor",})
 
+      start_all()
 
+      oldGrafana.wait_for_open_port(3500)
+      newGrafana.wait_for_open_port(3000)
+      newGrafana.wait_for_unit("grafana.service")
+      oldGrafana.wait_for_unit("grafana.service")
+      client.succeed("ping -c 2 newGrafana")
+      client.succeed("ping -c 2 oldGrafana")
 
-       authHeader = shlex.quote(f"Authorization: Bearer {token}")
-       start_all()
+      client.succeed("curl -v http://newGrafana:3000/api/health")
+      client.succeed("curl -v http://oldGrafana:3500/api/health")
 
-       oldGrafana.wait_for_open_port(3500)
-       newGrafana.wait_for_open_port(3000)
-       newGrafana.wait_for_unit("grafana.service")
-       oldGrafana.wait_for_unit("grafana.service")
-       client.succeed("ping -c 2 newGrafana")
-       client.succeed("ping -c 2 oldGrafana")
-
-       client.succeed("curl -v http://newGrafana:3000/api/health")
-       client.succeed("curl -v http://oldGrafana:3500/api/health")
-
-       newGrafanaSA = json.loads(
-                    client.succeed(
-                       f"curl -X POST -H Content-Type:application/json -d '{createReadSA}' -u admin:admin http://newGrafana:3000/api/serviceaccounts"))
-       oldGrafanaSA =  json.loads(client.succeed(
-                       f"curl -X POST -H Content-Type:application/json -d '{createReadWriteSA}' -u admin:admin http://oldGrafana:3500/api/serviceaccounts"))
+      newGrafanaSA = json.loads(
+                   client.succeed(
+                      f"curl -X POST -H Content-Type:application/json -d '{createReadSA}' -u admin:admin http://newGrafana:3000/api/serviceaccounts"))
+      oldGrafanaSA =  json.loads(client.succeed(
+                      f"curl -X POST -H Content-Type:application/json -d '{createReadWriteSA}' -u admin:admin http://oldGrafana:3500/api/serviceaccounts"))
 
       newGrafanaAuthHeader = shlex.quote(f"Authorization: Bearer {newGrafanaSA}")
+      newFolder = json.dumps({
+                 "uid": "nErXDvCkzz",
+                 "title": "Department ABC"})
 
 
+      oldGrafanaNewFolder =  json.loads(client.succeed(
+                      f"curl -X POST -H Content-Type:application/json -d '{newFolder}' -u admin:admin http://oldGrafana:3500/api/folders"))
+      assert oldGrafanaNewFolder.get("uid") == "nErXDvCkzz"
     '';
+
   };
 }
