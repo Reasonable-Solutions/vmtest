64a1f9813c84034b4d939e6c9da31baa18efd6fc
car <carl.hedgren@gmail.com>
2023-10-09 13:30:08 +0200

    do Service accounts

--- vm-test.nix
+++ vm-test.nix
@@ -58,7 +58,7 @@ in {
       import shlex
       createReadSA = json.dumps({
                  "name": "newGrafanaSA",
-                 "role": "Viewer",})
+                 "role": "Editor",})
       createReadWriteSA = json.dumps({
                  "name": "newGrafanaSA",
                  "role": "Editor",})
@@ -75,21 +75,24 @@ in {
       client.succeed("curl -v http://newGrafana:3000/api/health")
       client.succeed("curl -v http://oldGrafana:3500/api/health")
 
-      newGrafanaSA = json.loads(
-                   client.succeed(
-                      f"curl -X POST -H Content-Type:application/json -d '{createReadSA}' -u admin:admin http://newGrafana:3000/api/serviceaccounts"))
-      oldGrafanaSA =  json.loads(client.succeed(
-                      f"curl -X POST -H Content-Type:application/json -d '{createReadWriteSA}' -u admin:admin http://oldGrafana:3500/api/serviceaccounts"))
+      newGrafanaSA = client.succeed(
+                      f"curl --fail  -X POST -H Content-Type:application/json -d '{createReadSA}' -u admin:admin http://newGrafana:3000/api/auth/keys | jq -r .key ")
+      oldGrafanaSA = client.succeed(
+                      f"curl --fail -X POST -H Content-Type:application/json -d '{createReadWriteSA}' -u admin:admin http://oldGrafana:3500/api/auth/keys | jq -r .key")
 
       newGrafanaAuthHeader = shlex.quote(f"Authorization: Bearer {newGrafanaSA}")
+      oldGrafanaAuthHeader = shlex.quote(f"Authorization: Bearer {oldGrafanaSA}")
+
       newFolder = json.dumps({
-                 "uid": "nErXDvCkzz",
                  "title": "Department ABC"})
 
+      SA = client.succeed(f"curl --fail -X POST -H Content-Type:application/json -d '{createReadWriteSA}' http://admin:admin@oldGrafana:3500/api/serviceaccounts | jq -r .id")
+      SATREQ = json.dumps({"name": "my-secret"})
+      SAT = client.succeed(f"curl --fail -X POST -H Content-Type:application/json -d '{SATREQ}' http://admin:admin@oldGrafana:3500/api/serviceaccounts/2/tokens | jq -r .key")
+      SATHeader = shlex.quote(f"Authorization: Bearer {SAT}")
 
-      oldGrafanaNewFolder =  json.loads(client.succeed(
-                      f"curl -X POST -H Content-Type:application/json -d '{newFolder}' -u admin:admin http://oldGrafana:3500/api/folders"))
-      assert oldGrafanaNewFolder.get("uid") == "nErXDvCkzz"
+
+      client.succeed(f"curl -vv --fail -X POST  -H Content-Type:application/json -H Accept:application/json -d '{newFolder}' --url http://oldGrafana:3500/api/folders -H {SATHeader}")
     '';
 
   };
